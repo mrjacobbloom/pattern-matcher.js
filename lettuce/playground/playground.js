4c7386cb925fd695183c4ff2731a716bf3e4af4c
Jacob Bloom <mr.jacob.bloom@gmail.com>
2018-11-15 13:35:30 -0700

    namespace some of this ui code because it's horrendous

--- lettuce/playground/playground.js
+++ lettuce/playground/playground.js
@@ -6,25 +6,12 @@ import {renderTree, centerTree} from './renderTree.js';
 import {setErrSource} from '../errors.mjs';
 import {lettuceHighlightMode} from './lettuceHighlightMode.js'
 
-const grammar2 = nearley.Grammar.fromCompiled(grammar);
-
-let editor, ast, stepFunc, nodeMap = [], rightColSplit;
+let editor, ui = {}, rightColSplit;
 
 let logErr = (msg = '') => {
-  document.querySelector('#results').innerHTML = '';
-  document.querySelector('#errors').textContent = msg.stack || String(msg)
+  ui.results.innerHTML = '';
+  ui.errors.textContent = msg.stack || String(msg)
 };
-
-let selectNodeInSource = node => {
-  let loc = JSON.parse(node.getAttribute('data-loc'));
-  let [[a, b], [c, d]] = loc;
-  // moo values are 1-indexed, ace ranges are 0-indexed
-  let range = new ace.Range(a - 1, b - 1, c - 1, d - 1);
-  // seems like this successfully doesn't mess with other selections
-  // even when they overlap or contain the new range! Good work, ace
-  editor.addSelectionMarker(range);
-  return () => editor.removeSelectionMarker(range);
-}
 
 let valueToHTML = value => {
   if(!value) return '';
@@ -64,160 +51,190 @@ let renderEnv = (env, store) => {
   storeArea.innerHTML = html;
 }
 
-let deselectFunc;
-let highlightNode = term => {
-  document.querySelectorAll('.rendered-tree-node.highlight').forEach(node => node.classList.remove('highlight'));
-  if(deselectFunc) deselectFunc();
-  if(term) {
-    let nodeID = nodeMap.indexOf(term);
-    if(nodeID == -1) return;
-    let node = document.querySelector(`#rendered-tree div[data-node-id="${nodeID}"]`);
-    if(!node) return;
-    node.classList.add('highlight');
-    deselectFunc = selectNodeInSource(node);
+let tree = {
+  deselectFunc: null,
+  nodeMap: [],
+  selectNodeInSource: node => {
+    let loc = JSON.parse(node.getAttribute('data-loc'));
+    let [[a, b], [c, d]] = loc;
+    // moo values are 1-indexed, ace ranges are 0-indexed
+    let range = new ace.Range(a - 1, b - 1, c - 1, d - 1);
+    // seems like this successfully doesn't mess with other selections
+    // even when they overlap or contain the new range! Good work, ace
+    editor.addSelectionMarker(range);
+    return () => editor.removeSelectionMarker(range);
+  },
+  highlightNode: term => {
+    document.querySelectorAll('.rendered-tree-node.highlight').forEach(node => node.classList.remove('highlight'));
+    if(tree.deselectFunc) tree.deselectFunc();
+    if(term) {
+      let nodeID = tree.nodeMap.indexOf(term);
+      if(nodeID == -1) return;
+      let node = document.querySelector(`#rendered-tree div[data-node-id="${nodeID}"]`);
+      if(!node) return;
+      node.classList.add('highlight');
+      tree.deselectFunc = tree.selectNodeInSource(node);
+    }
+  },
+  render: ast => {
+    tree.nodeMap = renderTree(ast);
   }
 }
-let step = () => {
-  try {
-    if(stepFunc) {
-      let thunk = stepFunc();
-      if(thunk instanceof Thunk) {
-        if(playState == PLAY_STATES.SKIP) {
-          if(Date.now() - skipStartTime > 2000) {
-            setPlayState(PLAY_STATES.PAUSE);
-            throw 'Execution timed out';
+
+let playStates = {
+  STATES: {
+    PLAY: 1,
+    PAUSE: 2,
+    SKIP: 3
+  },
+  skipStartTime: null,
+  state: 2,
+  interval: null,
+  set: _state => {
+    playStates.state = _state;
+    switch(playStates.state) {
+      case playStates.STATES.PAUSE: {
+        ui.buttons.run.innerHTML = '<i class="icon-play"></i>';
+        if(playStates.interval !== null) {
+          clearInterval(playStates.interval);
+          playStates.interval = null;
+        }
+        playStates.skipStartTime = null;
+        break;
+      }
+      case playStates.STATES.PLAY: {
+        ui.buttons.run.innerHTML = '<i class="icon-pause"></i>';
+        if(playStates.interval !== null) clearInterval(playStates.interval);
+        playStates.interval = setInterval(interpreter.step, 150);
+        playStates.skipStartTime = null;
+        break;
+      }
+      case playStates.STATES.SKIP: {
+        ui.buttons.run.innerHTML = '<i class="icon-pause"></i>';
+        if(playStates.interval !== null) clearInterval(playStates.interval);
+        playStates.skipStartTime = Date.now();
+        interpreter.step();
+        break;
+      }
+    }
+  }
+};
+
+let interpreter = {
+  stepFunc: null,
+  ast: null,
+  step: () => {
+    try {
+      if(interpreter.stepFunc) {
+        let thunk = interpreter.stepFunc();
+        if(thunk instanceof Thunk) {
+          if(playStates.state == playStates.STATES.SKIP) {
+            if(Date.now() - playStates.skipStartTime > 2000) {
+              playStates.set(playStates.STATES.PAUSE);
+              throw 'Execution timed out';
+            }
+            interpreter.step();
+          } else {
+            tree.highlightNode(thunk.term);
+            renderEnv(thunk.env, thunk.store);
+            ui.results.innerHTML = '';
           }
-          step();
         } else {
-          highlightNode(thunk.term);
-          renderEnv(thunk.env, thunk.store);
-          document.querySelector('#results').innerHTML = '';
+          tree.highlightNode(null);
+          renderEnv(null, null);
+          playStates.set(playStates.STATES.PAUSE);
         }
       } else {
-        highlightNode(null);
-        renderEnv(null, null);
-        setPlayState(PLAY_STATES.PAUSE);
+        return;
       }
-    } else {
-      return;
+    } catch(err) {
+      logErr(err)
     }
-  } catch(err) {
-    logErr(err)
+  },
+  genStepFunc: result => {
+    logErr();
+    ui.results.innerHTML = valueToHTML(result);
+    playStates.set(playStates.STATES.PAUSE);
+    interpreter.stepFunc = stepThrough(interpreter.ast, interpreter.genStepFunc); // So you can play it a second time
+    return;
   }
-}
-
-const PLAY_STATES = {
-  PLAY: 1,
-  PAUSE: 2,
-  SKIP: 3
 };
-let skipStartTime;
-let playState = PLAY_STATES.PAUSE;
-let playInterval = null;
-function setPlayState(state) {
-  let runButton = document.querySelector('#run');
-  playState = state;
-  switch(state) {
-    case PLAY_STATES.PAUSE: {
-      runButton.innerHTML = '<i class="icon-play"></i>';
-      if(playInterval !== null) {
-        clearInterval(playInterval);
-        playInterval = null;
-      }
-      skipStartTime = null;
-      break;
-    }
-    case PLAY_STATES.PLAY: {
-      runButton.innerHTML = '<i class="icon-pause"></i>';
-      if(playInterval !== null) clearInterval(playInterval);
-      playInterval = setInterval(step, 150);
-      skipStartTime = null;
-      break;
-    }
-    case PLAY_STATES.SKIP: {
-      runButton.innerHTML = '<i class="icon-pause"></i>';
-      if(playInterval !== null) clearInterval(playInterval);
-      skipStartTime = Date.now();
-      step();
-      break;
-    }
-  }
-}
-let genStepFunc = result => {
-  logErr();
-  document.querySelector('#results').innerHTML = valueToHTML(result);
-  setPlayState(PLAY_STATES.PAUSE);
-  stepFunc = stepThrough(ast, genStepFunc); // So you can play it a second time
-  return;
-}
 
-let sourceUpdated = () => {
-  let data = editor.getValue();
-  let parser = new nearley.Parser(grammar2);
-  logErr();
-  if(deselectFunc) deselectFunc();
-  setPlayState(PLAY_STATES.PAUSE);
-  setErrSource(data);
-  try {
+let parser = {
+  grammar2: nearley.Grammar.fromCompiled(grammar),
+  parse: data => {
     if(!data.length) {
       throw '';
     }
-    parser.feed(data);
-    if(!parser.results.length) {
+    let _parser = new nearley.Parser(parser.grammar2);
+    _parser.feed(data);
+    if(!_parser.results.length) {
       throw 'Parse error';
     }
-    ast = parser.results[0];
-    nodeMap = renderTree(ast);
-    genStepFunc();
+    return _parser.results[0];
+  }
+};
+
+let sourceUpdated = () => {
+  let data = editor.getValue();
+  logErr();
+  if(tree.deselectFunc) tree.deselectFunc();
+  playStates.set(playStates.STATES.PAUSE);
+  setErrSource(data);
+  try {
+    interpreter.ast = parser.parse(data);
+    tree.render(interpreter.ast);
+    interpreter.genStepFunc();
   } catch(e) {
-    console.log(ast);
-    renderTree(null);
-    nodeMap = [];
-    stepFunc = null;
+    console.log(interpreter.ast);
+    tree.render(null)
+    tree.nodeMap = [];
+    interpreter.stepFunc = null;
     logErr(e);
   }
 };
 
+let splitDefaults = {
+  gutterSize: 10,
+  elementStyle: (dimension, size, gutterSize) => ({
+    'flex-basis': `calc(${size}% - ${gutterSize}px)`,
+  }),
+  gutterStyle: (dimension, gutterSize) => ({
+      'flex-basis':  `${gutterSize}px`,
+  }),
+};
+
 window.addEventListener('load', () => {
+  ui.results = document.querySelector('#results');
+  ui.errors = document.querySelector('#errors');
+  ui.buttons = {
+    run: document.querySelector('#run'),
+    step: document.querySelector('#step'),
+    skip: document.querySelector('#skip')
+  };
+
   // Set up the draggy gutters
   Split(['#col-1', '#col-2', '#col-3'], {
-    gutterSize: 10,
+    ...splitDefaults,
     sizes: [20, 60, 20],
-    elementStyle: (dimension, size, gutterSize) => ({
-      'flex-basis': `calc(${size}% - ${gutterSize}px)`,
-    }),
-    gutterStyle: (dimension, gutterSize) => ({
-        'flex-basis':  `${gutterSize}px`,
-    }),
     onDrag: centerTree
   });
   rightColSplit = Split(['#col-3-top', '#col-3-middle', '#col-3-bottom'], {
-    gutterSize: 10,
+    ...splitDefaults,
     sizes: [30, 70, 0],
     minSize: [20, 20, 0],
-    elementStyle: (dimension, size, gutterSize) => ({
-      'flex-basis': `calc(${size}% - ${gutterSize}px)`,
-    }),
-    gutterStyle: (dimension, gutterSize) => ({
-        'flex-basis':  `${gutterSize}px`,
-    }),
     direction: 'vertical'
   });
 
   // initialize the editor
   let defaultProgramsSelect = document.querySelector('#default-programs');
-  let first = true;
   for(let [name, value] of Object.entries(defaultPrograms)) {
     let option = document.createElement('option');
     option.value = value;
     option.innerText = name;
-    if(first) { // this is hacky, but idc
-      first = false;
-      option.selected = 'selected';
-      document.querySelector('#editor').textContent = value;
-    }
     defaultProgramsSelect.appendChild(option);
   }
+  document.querySelector('#editor').textContent = defaultProgramsSelect.value;
   editor = ace.edit('editor');
   editor.session.on('change', sourceUpdated);
   editor.session.setMode(lettuceHighlightMode);
@@ -226,31 +243,28 @@ window.addEventListener('load', () => {
   })
 
   // initialize parse tree and stuff
-  let tree = document.querySelector('#rendered-tree');
-  tree.addEventListener('mouseover', e => {
+  tree.elem = document.querySelector('#rendered-tree');
+  tree.elem.addEventListener('mouseover', e => {
     if(e.target.hasAttribute('data-loc')) {
-      let deselectFunc = selectNodeInSource(e.target);
-      e.target.addEventListener('mouseout', deselectFunc, {once: true})
+      tree.deselectFunc = tree.selectNodeInSource(e.target);
+      e.target.addEventListener('mouseout', tree.deselectFunc, {once: true})
     }
   });
   sourceUpdated();
 
   // set up 3rd column buttns and stuff
-  let runButton = document.querySelector('#run');
-  let stepButton = document.querySelector('#step');
-  let skipButton = document.querySelector('#skip');
-  runButton.addEventListener('click', () => {
-    if(playState == PLAY_STATES.PAUSE) {
-      setPlayState(PLAY_STATES.PLAY);
+  ui.buttons.run.addEventListener('click', () => {
+    if(playStates.state == playStates.STATES.PAUSE) {
+      playStates.set(playStates.STATES.PLAY);
     } else {
-      setPlayState(PLAY_STATES.PAUSE);
+      playStates.set(playStates.STATES.PAUSE);
     }
   });
-  stepButton.addEventListener('click', () => {
-    setPlayState(PLAY_STATES.PAUSE);
-    step();
+  ui.buttons.step.addEventListener('click', () => {
+    playStates.set(playStates.STATES.PAUSE);
+    interpreter.step();
   });
-  skipButton.addEventListener('click', () => {
-    setPlayState(PLAY_STATES.SKIP);
+  ui.buttons.skip.addEventListener('click', () => {
+    playStates.set(playStates.STATES.SKIP);
   });
 });
